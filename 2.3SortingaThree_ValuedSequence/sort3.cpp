c23630c7518ac074010f15e36dc96519fa8dede2
a.kanter <a.kanter@robotcaresystems.com>
2017-06-14 23:33:48 +0100

    sort4

--- 2.3SortingaThree_ValuedSequence/sort3.cpp
+++ 2.3SortingaThree_ValuedSequence/sort3.cpp
@@ -2,7 +2,7 @@
 
 /*
 ID: alexisk2
-PROG: frac1
+PROG: sort3
 LANG: C++11
 */
 
@@ -22,28 +22,61 @@ using namespace std;
 
 int find_smallest(vector<int> num, int pos){
    int i=pos-1;
-   if (num[i]<num[pos]){
-      i--;
-   }else{ 
-      return i+1;
+//   cout <<num[pos]<<"  "<<pos<<"  \n";
+   if (num[pos]==1){
+      int min_pos3=pos;
+      for(int i=pos-1; i>=0; i--){
+         if(num[i]==3){
+            min_pos3=i;
+         }
+      }
+  //    cout<<"min3  "<<min_pos3<<"\n";
+      int min_pos2=pos;
+      if(min_pos3==pos){
+        for(int i=pos-1; i>=0; i--){
+          if(num[i]==2){
+            min_pos2=i;
+          }
+         }
+    //   cout<<"min2 "<<min_pos2<<"\n";
+      }else {
+      // cout<<"AA"<<" \n"; 
+       return min_pos3;
+      }
+      return min_pos2;
    }
+  if (num[pos]==2){
+      int min_pos3=pos;
+      for(int i=pos-1; i>=0; i--){
+         if(num[i]==3){
+            min_pos3=i;
+         }
+      }
+     // cout<<"min2 "<<min_pos3<<"\n";
+      return min_pos3;
+   }
+
    return pos;
 } 
 
 void sort_next(vector<int> num, int pos , int *changes ){
     if(pos>0){
-        cout<<pos<<"\n";
+       // cout<<pos<<" pppp "<<*changes<<"\n";
         if(num[pos]==3){
            sort_next(num, pos-1 ,changes);
         }else{
 	   int sm=find_smallest(num,pos);
            if (sm !=pos){
-               cout<<"Q!! SM "<<sm<<"\n";
-		   changes++;
+         //      cout<<"Q!! SM "<<sm<<"\n";
+		    (*changes)++;
+	           cout<<"CHANGES  "<<*changes<<"\n "<<sm<< "  "<<pos<<"\n";
 		   int  c= num[sm];
 		   num[sm]=num[pos];
 		   num[pos]=c; 
            }
+
+     //for(int i=0; i<num.size();i++) cout<<"\n"<<num[i]<<"\n";
+       // cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
            sort_next(num,pos-1,changes);
         }
          
@@ -62,8 +95,13 @@ int main() {
          ifs>>k;
          num.push_back(k);      
    }
+
+   for(int i=0; i<num.size();i++) cout<<"\n"<<num[i]<<"\n";
    int changes = 0;
    sort_next(num,num.size()-1, &changes);
-   ofs<<changes;
+   cout<<"after\n"; 
+  
+   for(int i=0; i<num.size();i++) cout<<"\n"<<num[i]<<"\n";
+   ofs<<changes<<"\n";
 }
 
